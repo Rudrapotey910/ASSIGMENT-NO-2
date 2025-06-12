9049c90d8c43e6897dfb7113ae1ebaf07c6cd5a8
Rudrapotey910 <rudrapotey910@gmail.com>
2025-06-12 22:05:42 +0530

    Update file.py

--- file.py
+++ file.py
@@ -4,14 +4,7 @@ if x%2==0:
     print(f"{x} is an even  number")
 else:
     print(f"{x} is an odd number")
-
-
-
-
-
-
-
-
+#Task 2
 x=sum(range(1,51))
 print(f"{x} is the sum of numbers from 1 to 50")
 
