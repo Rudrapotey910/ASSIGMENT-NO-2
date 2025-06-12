e2bf662d8146dff27f5e4c303365c962367b7487
Rudrapotey910 <rudrapotey910@gmail.com>
2025-06-12 17:04:00 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,20 @@
+#Task 1
+x=int(input('enter the number'))
+if x%2==0:
+    print(f"{x} is an even  number")
+else:
+    print(f"{x} is an odd number")
+
+
+
+
+
+
+
+
+x=sum(range(1,51))
+print(f"{x} is the sum of numbers from 1 to 50")
+
+   
+
+
